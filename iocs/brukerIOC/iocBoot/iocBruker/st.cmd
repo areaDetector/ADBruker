2e9923e8efe4c738fe45069356b574e9bb60971e
Mark Rivers <rivers@cars.uchicago.edu>
2015-03-29 19:51:17 -0500

    Changes for ADCore R2-2

--- iocs/brukerIOC/iocBoot/iocBruker/st.cmd
+++ iocs/brukerIOC/iocBoot/iocBruker/st.cmd
@@ -4,15 +4,25 @@ errlogInit(20000)
 dbLoadDatabase("$(TOP)/dbd/BISDetectorApp.dbd")
 BISDetectorApp_registerRecordDeviceDriver(pdbbase) 
 
+# Prefix for all records
 epicsEnvSet("PREFIX", "BIS:")
+# The port name for the detector
 epicsEnvSet("PORT",   "APX")
 epicsEnvSet("COMMAND_PORT", "BIS_COMMAND")
 epicsEnvSet("FILE_PORT",    "BIS_FILE")
 epicsEnvSet("STATUS_PORT",  "BIS_STATUS")
+# The queue size for all plugins
 epicsEnvSet("QSIZE",  "20")
+# The maximim image width; used for row profiles in the NDPluginStats plugin
 epicsEnvSet("XSIZE",  "512")
+# The maximim image height; used for column profiles in the NDPluginStats plugin
 epicsEnvSet("YSIZE",  "512")
+# The maximum number of time seried points in the NDPluginStats plugin
 epicsEnvSet("NCHANS", "2048")
+# The maximum number of frames buffered in the NDPluginCircularBuff plugin
+epicsEnvSet("CBUFFS", "500")
+# The search path for database files
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 
 ###
 # Create the asyn port to talk to the BIS server on command port 49153.
@@ -37,8 +47,6 @@ asynOctetSetInputEos($(COMMAND_PORT), 0, "]")
 asynOctetSetInputEos($(STATUS_PORT), 0, "\n")
 
 BISDetectorConfig($(PORT), $(COMMAND_PORT), $(STATUS_PORT), 0, 0)
-dbLoadRecords("$(ADCORE)/db/ADBase.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(ADCORE)/db/NDFile.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(ADBRUKER/db/BIS.template",    "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1,BIS_PORT=$(COMMAND_PORT)")
 asynSetTraceIOMask($(PORT),0,2)
 #asynSetTraceMask($(PORT),0,255)
@@ -46,9 +54,8 @@ asynSetTraceIOMask($(PORT),0,2)
 
 # Create a standard arrays plugin
 NDStdArraysConfigure("apxImage", 5, 0, $(PORT), 0, 0)
-dbLoadRecords("$(ADCORE)/db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=apxImage,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
 # This creates a waveform large enough to hold a 4096x4096 image
-dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=apxImage,ADDR=0,TIMEOUT=1,TYPE=Int32,FTVL=LONG,NELEMENTS=16777216")
+dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=apxImage,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int32,FTVL=LONG,NELEMENTS=16777216")
 
 # Load all other plugins using commonPlugins.cmd
 < $(ADCORE)/iocBoot/commonPlugins.cmd
